59e3f0f15025affa7d81a498149c0e287716567d
WISSAL MN <135167709+WissalManseri@users.noreply.github.com>
2023-06-02 17:20:23 +0100

    Add files via upload

---
+++ Python Math.py
@@ -0,0 +1,20 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+a = abs(-7.25)
+print(a)
+w = pow(4, 3)
+print(w)
+#Import math library
+import math
+r = math.sqrt(64)
+print(r)
+#Round a number upward to its nearest integer
+i= math.ceil(1.4)
+#Round a number downward to its nearest integer
+s = math.floor(1.4)
+print(s)
+print(i)
+y = math.pi
+print(y)
